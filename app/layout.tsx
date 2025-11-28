70a986f1cadca47a5287650d3c9655115388ef47
RodyUzuriaga <rody.uzuriaga@usil.pe>
2025-11-28 07:14:05 -0500

    Agregar logo H para el navegador (favicon)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,20 +14,7 @@ export const metadata: Metadata = {
     "Plataforma de automatización del control de calidad con IA para la producción de jeans en Confecciones HAM'S",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: "/h-logo.svg",
     apple: "/apple-icon.png",
   },
 }
